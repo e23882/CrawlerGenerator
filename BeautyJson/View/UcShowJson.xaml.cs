4be90a081bb44b1ca661a559f45dd280488ace0c
Leoyang <leoyang@jepun.com.tw>
2020-08-27 19:58:34 +0800

    update

---
+++ BeautyJson/View/UcShowJson.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace BeautyJson.View
+{
+    public partial class UcShowJson : Window
+    {
+        public UcShowJson()
+        {
+            InitializeComponent();
+        }
+    }
+}
